6ecf3c9666fa447bc1730c8a4fb9dcd2283efedd
Jonathan <Spircoff_j@yahoo.com>
2022-10-27 12:10:07 -0400

    added index.js with inquirer and functions

---
+++ src/index.js
@@ -0,0 +1,66 @@
+const inquirer = require('inquirer')
+// const htmlGenerator = require()
+
+inquirer
+    .prompt([
+    {
+        name: ,
+        type: 'input',
+        message: , 
+
+
+    },
+    {
+        name: ,
+        type: 'input',
+        message: , 
+
+
+    },
+    {
+        name: ,
+        type: 'input',
+        message: , 
+
+
+    },
+    {
+        name: ,
+        type: 'input',
+        message: , 
+
+
+    },
+    {
+        name: ,
+        type: 'input',
+        message: , 
+
+
+    },
+    {
+        name: ,
+        type: 'input',
+        message: , 
+
+
+    },
+    {
+        name: ,
+        type: 'input',
+        message: , 
+
+
+    }
+])
+.then((answers) => {
+    console.log(answers)
+
+});
+
+    function init(){
+
+
+    }
+
+    init();
