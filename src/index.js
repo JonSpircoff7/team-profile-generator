24f8d430b91f239108cf7958bed27bc88d70244e
Jonathan <Spircoff_j@yahoo.com>
2022-10-28 19:51:05 -0400

    added txt file with pseudo code and guidelines

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 const inquirer = require('inquirer')
-// const htmlGenerator = require()
 
+const {writeFile} = require('fs').promises;
+
+
+// Prompt the questions
 const promptUser = () => {
     return inquirer.prompt([
     {
         name: ,
         type: 'input',
-        message: , 
+        message: 'Are you an employee at RealBuzz?', 
 
 
     },
     {
         name: ,
         type: 'input',
-        message: , 
+        message: 'What is your role?', 
 
 
     },
     {
         name: ,
         type: 'input',
-        message: , 
+        message: 'What is your name?', 
 
 
     },
     {
         name: ,
         type: 'input',
-        message: , 
+        message: 'What is your social media?', 
 
 
     },
@@ -62,7 +65,7 @@ const generateHTML = ({ engineer, intern, manager}) =>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
+  <title>Team Profile Generator</title>
 </head>
 <body>
   </div>
@@ -70,6 +73,7 @@ const generateHTML = ({ engineer, intern, manager}) =>
 </body>
 </html>`;
 
+// Create the HTML file with the data received
     const init = () => {
     promptUser()
         .then((answer) => writeFile('index.html', generateHTML(answers)))
